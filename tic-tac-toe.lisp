8405e511c489efea3504797c1606ab4941b03585
Aptenodyte <graywater.stables@zoho.com>
2013-12-03 22:54:20 -0500

    just a warm-up, I hope...

---
+++ tic-tac-toe.lisp
@@ -0,0 +1,50 @@
+;; this is a simple game of tic-tac-toe
+;; first graphical, then non-graphical
+
+;; this is a stateful implementation
+(defparameter *game-state* (make-array '(3 3) :initial-contents 
+				       '(((nil) (nil) (nil))
+					 ((nil) (nil) (nil))
+					 ((nil) (nil) (nil)))
+				       )
+  "this is the game of tic-tac-toe"
+  )
+
+; access with (aref symbol x y
+; remember, 0 is the base coord!
+
+(defstruct player name mark)
+; mark can be X or O
+
+(defun mark-spot! (mark x y) ; make the Schemers feel at home
+  "marks a square"
+  (setf (aref x y) mark))
+; make sure to decf x and y in the final release, so that we can have 1-3 instead of 0-2
+
+(
+
+(defun ticp (x y z)
+  "tests if tic-tac-toe!"
+  (and (eq x y) (eq y z)))
+
+(defun winp ())
+
+#|
+(defmacro eq3 (x y z)
+  `(and (eq x y) (eq y z)))
+|#
+
+
+#|
+(loop for i
+     from 0
+     do (loop for i
+	     from 0
+	     do
+	     until 2
+     until 2
+|#
+
+(defun game ()
+  "a simple game of tic-tac-toe"
+  )
